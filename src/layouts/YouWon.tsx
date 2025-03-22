0b68c6e4deb10db85fe42443f0b4a5cce51df5b0
reinisriekstins <riekstins.reinis@gmail.com>
2025-03-22 13:35:20 +0200

    more examples in "YouWon" section

--- src/layouts/YouWon.tsx
+++ src/layouts/YouWon.tsx
@@ -1,35 +1,57 @@
 import { Heading } from '@radix-ui/themes'
-import { animated, useSpring } from '@react-spring/web'
+import { animated, useSpring, to } from '@react-spring/web'
 import React, { useEffect } from 'react'
+import { useMove } from '@use-gesture/react'
 
+// #1. You can animate custom components by wrapping
+// them into `animated(...)`
 const AnimatedHeading = animated(Heading);
 
 const YouWon = () => {
-  const [{ amount }, amountApi] = useSpring(() => ({
+  const [{ amount, color }, amountApi] = useSpring(() => ({
+    // #2. you can pass strings and even CSS variables
+    // into spring configs, as long as its possible
+    // to resolve it into a number
+    color: 'white', // #FFFFFF or rgb(255,255,255)
     amount: 0,
   }));
 
   useEffect(() => {
-    amountApi.start({ amount: 1000000 });
+    amountApi.start({
+      amount: 1000000,
+      color: 'red', // #FF0000 or rgb(255,0,0)
+    });
   }, []);
+
+  const { y: mouseY } = useMouseMoveSpring();
 
   return (
     <div style={{ display: 'grid', placeItems: 'center' }}>
       <Heading mb="2" size="4">You won!</Heading>
 
-      <AnimatedHeading mb="2" size="7">
+      <AnimatedHeading mb="2" size="7" style={{ color }}>
         {/* 
-          #1. The consumed spring value can be not only style properties,
+          #3. The consumed spring value can be not only style properties,
           but also any prop that accepts a numeric value and also "children".
         */}
-        {amount}
+        {/* {amount} */}
 
         {/*
-          #2. To transform any numeric spring value and add any
+          #4. To transform any numeric spring value and add any
           formatting to it you can use the .to(...) method.
           In this case we're removing decimal digits and adding the "€" symbol.
         */}
         {/* {amount.to(amountNumber => `${Math.floor(amountNumber)} €`)} */}
+
+        {/* #5. Merge output from multiple springs into one */}
+        {/* {to(
+          [amount, mouseY],
+          (amountNumber, mouseYNumber) => {
+            console.log('mouseYNumber', mouseYNumber);
+
+            return `${Math.floor(amountNumber * mouseYNumber)} €`;
+          }
+        )} */}
       </AnimatedHeading>
 
     </div>
@@ -37,3 +59,18 @@ const YouWon = () => {
 }
 
 export default YouWon
+
+const useMouseMoveSpring = () => {
+  const [mouseSprings, api] = useSpring(() => ({ x: 0, y: 0 }))
+
+  useMove(
+    ({ xy: [x, y] }) => {
+      api.start({ x, y });
+    },
+    {
+      target: window, // Bind to the window
+    }
+  );
+
+  return mouseSprings;
+}
